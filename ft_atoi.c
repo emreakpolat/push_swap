8aceaef55582622bd070a0357f25b19b7d26ab73
makpolatt <emreakpolat3@gmail.com>
2025-02-07 18:31:44 +0300

    blablabla

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,12 +6,21 @@
 /*   By: makpolat <makpolat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/20 16:34:19 by makpolat          #+#    #+#             */
-/*   Updated: 2025/02/05 19:03:13 by makpolat         ###   ########.fr       */
+/*   Updated: 2025/02/07 12:52:39 by makpolat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pushswap.h"
-
+void maxcheck(long k, char *seperate[], int *ptr, int sign)
+{
+	k = (sign * k);
+	if ((k > 2147483647) || (k < -2147483648))
+	{
+		free_all(seperate);
+		free(ptr);
+		error("Sayi sinirlarin disindaydi\n"); // Error'a girerse exit ile çıkar ve program sonlanır
+	}
+}
 int	ft_atoi(const char *str, int *ptr, char **seperate)
 {
 	long	k;
@@ -21,22 +30,19 @@ int	ft_atoi(const char *str, int *ptr, char **seperate)
 	sign = 1;
 	while (*str == ' ' || (*str >= '\t' && *str <= '\r'))
 		str++;
-	if (*str == '-' || *str == '+')
+	if (*str == '-' || *str == '+') 
 	{
 		if (*str == '-')
 			sign = -1;
 		str++;
 	}
+	if (*str == 0)
+		error("bos isaret gimeee\n");
 	while (ft_isdigit(*str) == 1)
 	{
 		k = k * 10 + (*str - 48);
+		maxcheck(k, seperate, ptr, sign);
 		str++;
-		if ((k > 2147483647) || (k < -2147483648))
-		{
-			free_all(seperate);
-			free(ptr);
-			error("Sayi sinirlarin disindaydi\n"); // Error'a girerse exit ile çıkar ve program sonlanır
-		}
 	}
 	return (sign * k);
 }
