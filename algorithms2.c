21e968ce66eb3e1146afbef372caf2e334a0a5ee
makpolatt <emreakpolat3@gmail.com>
2025-03-02 10:57:04 +0300

    radix sort bitti dosyalandı

--- algorithms2.c
+++ algorithms2.c
@@ -6,215 +6,237 @@
 /*   By: makpolat <makpolat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/01 11:02:45 by makpolat          #+#    #+#             */
-/*   Updated: 2025/03/01 13:00:32 by makpolat         ###   ########.fr       */
+/*   Updated: 2025/03/02 10:56:51 by makpolat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "pushswap.h"
 
 
-// void    position(t_general *stack)
+
+
+
+// t_list	*get_min(t_list *stack)
 // {
-//     int     i;
-//     t_list  *temp;
+// 	t_list	*min;
+// 	t_list	*node;
+// 	int		control;
+
+// 	node = stack;
+// 	min = NULL;
+// 	control = 0;
+// 	if (node)
+// 	{
+// 		while (node)
+// 		{
+// 			if (node->index == -1 && (!control || node->data < min->data))
+// 			{
+// 				min = node;
+// 				control = 1;
+// 			}
+// 			node = node->next;
+// 		}
+// 	}
+// 	return (min);
+// }
+
+// void	add_index(t_list *stack)
+// {
+// 	t_list	*node;
+// 	int		index;
+
+// 	index = 0;
+// 	node = get_min(stack);
+// 	while (node)
+// 	{
+// 		node->index = index++;
+// 		node = get_min(stack);
+// 	}
+// }
+
+
+// int	is_sorted(t_list *stack)
+// {
+// 	t_list	*node;
+
+// 	node = stack;
+// 	if (!node || !node->next)
+// 		return (1);
+// 	while (node->next)
+// 	{
+// 		if (node->data > node->next->data)
+// 			return (0);
+// 		node = node->next;
+// 	}
+// 	return (1);
+// }
+
+
+// t_list	*min_node(t_list *stack)
+// {
+// 	t_list	*tmp;
+// 	t_list	*min;
+
+// 	if (!stack || !stack)
+// 		return (NULL);
+// 	min = stack;
+// 	tmp = min->next;
+// 	while (tmp != NULL)
+// 	{
+// 		if (tmp->index < min->index)
+// 			min = tmp;
+// 		tmp = tmp->next;
+// 	}
+// 	return (min);
+// }
+
+
+// void	position(t_general *stack)
+// {
+// 	t_list	*node;
+
+// 	node = stack->a;
+// 	if (min_node(stack->a) == ft_lstlast(stack->a))
+// 		ft_rra(&stack->a);
+// 	else
+// 	{
+// 		while (min_node(stack->a) != node)
+// 		{
+// 			ft_ra(stack->a);
+//             write(1, "ra\n", 3);
+
+// 			node = stack->a;
+// 		}
+// 	}
+// 	ft_pb(stack);
+//     write(1, "pb\n", 3);
+
+// }
+
+// int	get_max_bits(t_list *stack)
+// {
+// 	t_list	*head;
+// 	int		max;
+// 	int		max_bits;
+
+
+// 	head = stack;
+// 	max = head->index;
+// 	max_bits = 0;
+// 	while (head)
+// 	{
+// 		if (head->index > max)
+// 			max = head->index;
+// 		head = head->next;
+// 	}
+// 	while ((max >> max_bits) != 0)
+// 		max_bits++;
+// 	return (max_bits);
+// }
+
+// void threesort(t_general *stack)
+// {
+//     t_list *temp;
 
 //     temp = stack->a;
-//     i = 0;
-//     while (temp)
+
+//     if (temp->data > temp->next->data)
 //     {
-//         temp->position = i;
-//         temp = temp->next;
-//         i++;
+//         write(1, "sa\n", 3);
+//         ft_sa(stack);
 //     }
-//     temp = stack->b;
-//     i = 0;
-//     while (temp)
+//     temp = stack->a;
+//     if (temp->next->data > temp->next->next->data)
 //     {
-//         temp->position = i;
-//         temp = temp->next;
-//         i++;
+//         write(1, "rra\n", 4);
+//         ft_rra(&stack->a);
 //     }
-// }
-
-
-// void target_position_for_a(t_list *a, t_list *b)
-// {
-//     t_list *iter_a;
-//     t_list *iter_b;
-
-//     iter_a = b;
-//     while (iter_a)
+//     temp = stack->a;
+//     if (temp->next->data < temp->data)
 //     {
-//         int num = INT_MAX;
-//         iter_b = a;
-//         while (iter_b)
-//         {
-//             if (iter_a->data > iter_b->data && num > iter_b->data)
-//             {
-//                 num = iter_b->data;
-//                 iter_a->target_pos = iter_b->position;
-//             }
-//             iter_b = iter_b->next;
-//         }
-//         if (num == INT_MAX)
-//         {
-//             iter_b = b;
-//             num = iter_b->data;
-//             iter_a->target_pos = iter_b->position;
-//             while (iter_b)
-//             {
-//                 if (iter_b->data < num)
-//                 {
-//                     num = iter_b->data;
-//                     iter_a->target_pos = iter_b->position;
-//                 }
-//                 iter_b = iter_b->next;
-//             }
-//         }
-//         iter_a = iter_a->next;
+//         write(1, "sa\n", 3);
+//         ft_sa(stack);
 //     }
 // }
 
-
-// void    cost_function_for_a(t_general *stack)
+// void	sort_4(t_general *stack)
 // {
-//     t_list *temp_a;
-//     int     size_b;
-//     int     size_a;
+// 	if (is_sorted(stack->a))
+// 		return ;
+// 	position(stack);
+// 	threesort(stack);
+// 	ft_pa(stack);
+//     write(1, "pa\n", 3);
 
-//     size_a = listsize(stack->a);
-//     size_b = listsize(stack->b);
-//     temp_a = stack->a;
-//     while (temp_a)
-//     {
-//         temp_a->a_cost_a = temp_a->position;
-//         temp_a->a_cost_b = temp_a->target_pos;
-//         if (temp_a->position > size_a / 2)
-//             temp_a->a_cost_a = (size_a - temp_a->position) * -1;
-//         if (temp_a->target_pos > size_b / 2)
-//             temp_a->a_cost_b = (size_b - temp_a->target_pos) * -1;
-//         printf("temp->data %d temp->a_cost_a: %d\n", temp_a->data, temp_a->target_pos);
-//         temp_a = temp_a->next;
-//     }
 // }
 
-// t_list  *findcheapest_for_a(t_general *stack)
+// void	sort_5(t_general *stack)
 // {
-//     int     i;
-//     int     j;
-//     int     num;
-//     t_list  *cheapest;
-//     t_list  *iter;
-    
-//     num = 2147483647;
-//     iter = stack->a;
-//     cheapest = iter;
-//     while (iter)
-//     {
-//         if (iter->a_cost_a < 0)
-//             i = (iter->a_cost_a * -1);
-//         if(iter->a_cost_b < 0)
-//             j = (iter->a_cost_b * -1);
-//         if ((i + j) < num)
-//         {
-//             num = (i + j);
-//             cheapest = iter;
-//         }
-//         iter = iter->next;
-//     }
-//     return (cheapest);
+// 	position(stack);
+// 	sort_4(stack);
+// 	ft_pa(stack);
+//     write(1, "pa\n", 3);
 // }
 
-
-// static void	double_rev_rotate(t_general *a, int *cost_a, int *cost_b)
+// void	radix(t_general *stack)
 // {
-// 	while (*cost_a < 0 && *cost_b < 0)
+// 	int		size;
+// 	int		max_bits;
+// 	int		i;
+// 	int		j;
+// 	t_list	*node;
+
+// 	size = listsize(stack->a);
+// 	max_bits = get_max_bits(stack->a);
+// 	i = 0;
+// 	while (i < max_bits)
 // 	{
-// 		(*cost_a)++;
-// 		(*cost_b)++;
-//         write(1, "rrr\n", 4);
-// 		operator(a, "rrr");
-// 	}
-// }
-// static void	double_rotate(t_general *stack, int *cost_a, int *cost_b)
-// {
-// 	while (*cost_a > 0 && *cost_b > 0)
-// 	{
-// 		(*cost_a)--;
-// 		(*cost_b)--;
-// 		ft_ra(stack->a);
-// 		ft_ra(stack->b);
-//         write(1, "rr\n", 3);
-// 	}
-// }
-// static void	rotate_a(t_general *stack, int *cost_b)
-// {
-// 	while (*cost_b)
-// 	{
-// 		if (*cost_b > 0)
+// 		j = 0;
+// 		while (j < size)
 // 		{
-//             write(1, "ra\n", 3);
-// 			ft_ra(stack->a);
-// 			(*cost_b)--;
+// 			node = stack->a;
+// 			if (((node->index >> i) & 1) == 1)
+//             {
+// 				ft_ra(stack->a);
+//                 write(1, "ra\n", 3);
+
+//             }
+// 			else
+//             {
+// 				ft_pb(stack);
+//                 write(1, "pb\n", 3);
+
+//             }
+//             j++;
 // 		}
-// 		else if (*cost_b < 0)
-// 		{
-//             write(1, "rra\n", 4);
-// 			ft_rra(&stack->a);
-// 			(*cost_b)++;
-// 		}
+// 		while (stack->b)
+//         {
+// 			ft_pa(stack);
+//             write(1, "pa\n", 3);
+//         }
+//         i++;
 // 	}
 // }
 
-// static void	rotate_b(t_general *stack, int *cost_a)
+
+
+// void sorting(t_general *stack)
 // {
-// 	while (*cost_a)
-// 	{
-//         if (*cost_a > 0)
-//         {
-//             write(1, "rb\n", 3);
-//             ft_ra(stack->b);
-// 		    (*cost_a)--;
-//         }
-//         else if(*cost_a < 0)
-//         {
-//             write(1, "rrb\n", 4);
-// 		    operator(stack, "rrb");
-//             (*cost_a)++;
-//         }
-//     }
-// }
-
-// void	move(t_general *stack, t_list *cheap)
-// {
-//     if (cheap->cost_a < 0 && cheap->cost_b < 0)
+// add_index(stack->a);
+// 	if (listsize(stack->a) < 2)
+// 		return ;
+// 	if (is_sorted(stack->a))
+// 		return ;
+// 	if (listsize(stack->a) == 2)
 //     {
-//         double_rev_rotate(stack, &cheap->cost_a, &cheap->cost_b);
+//         ft_sa(stack);
+//         write(1, "sa\n", 3);
 //     }
-//     else if (cheap->cost_a > 0 && cheap->cost_b > 0)
-//     {
-//         double_rotate(stack, &cheap->cost_a, &cheap->cost_b);
-//     }
-//     rotate_a(stack, &cheap->cost_a);
-//     rotate_b(stack, &cheap->cost_b);
-//     write(1, "pb\n", 3);
-// 	ft_pb(stack);
+// 	else if (listsize(stack->a) == 3)
+// 		threesort(stack);
+// 	else if (listsize(stack->a) == 4)
+// 		sort_4(stack);
+// 	else if (listsize(stack->a) == 5)
+// 		sort_5(stack);
+// 	else
+// 		radix(stack);
 // }
-
-
-// void pb_all(t_general *stack)
-// {
-//     t_list *cheap;
-//     ft_pb(stack);
-//     write(1 ,"pb\n", 3);
-//     while (listsize(stack->a) > 3)
-//     {
-//         cheap = findcheapest_for_a(stack);
-//         cost_function_for_a(stack);
-//         position(stack);
-//         target_position_for_a(stack->b, stack->a);
-//         cost_function_for_a(stack);
-//         move(stack, cheap);
-//     }
-// }
-
